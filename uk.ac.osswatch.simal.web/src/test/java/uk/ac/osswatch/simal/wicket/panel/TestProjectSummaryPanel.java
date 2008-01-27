0ea7bafb0e0079d34083c53f16ffe01b1a7ed1bc
rgardler@apache.org <rgardler@apache.org@cad361de-782e-0410-9987-355e4c56c873>
2008-01-27 22:38:02 +0000

    Add test for ProjectSummaryPanel

---
+++ uk.ac.osswatch.simal.web/src/test/java/uk/ac/osswatch/simal/wicket/panel/TestProjectSummaryPanel.java
@@ -0,0 +1,35 @@
+package uk.ac.osswatch.simal.wicket.panel;
+
+import javax.xml.namespace.QName;
+
+import junit.framework.TestCase;
+
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.util.tester.TestPanelSource;
+import org.apache.wicket.util.tester.WicketTester;
+
+/**
+ * Simple test using the WicketTester
+ */
+public class TestProjectSummaryPanel extends TestCase {
+	private WicketTester tester;
+
+	public void setUp() {
+		tester = new WicketTester();
+	}
+
+	@SuppressWarnings("serial")
+	public void testRenderUserHomePage() {
+		 tester.startPanel(new TestPanelSource()
+		 {
+		        public Panel getTestPanel(String panelId)
+		        {
+		                QName qname = new QName("http://simal.oss-watch.ac.uk/simalTest#");
+		                return new ProjectSummaryPanel(panelId, qname);
+		        }
+		 });
+		 // path = 1:panel:projectName.Label
+		tester.assertVisible("panel:projectName");
+		tester.assertLabel("panel:projectName", "Simal DOAP Test");
+	}
+}
