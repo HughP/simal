75a476e5cfb906b31f08d99d41bfba4860b0cfb5
rgardler@apache.org <rgardler@apache.org@cad361de-782e-0410-9987-355e4c56c873>
2008-01-29 23:47:49 +0000

    Add a wrapper for Elmo objects and convert all classes to use this wrapper instead.
    Simal code should not touch the Elmo DOAP class, instead they should do all work via these wrappers.
    
    The wrapper is required in order to provide easy access within the wicket framework, for example the Elmo class returns a Set of names, which, whilst correct makes it difficult to render well in the UI. This wrapper allows us to control which name will be returned.

---
+++ trunk/uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/model/IProject.java
@@ -0,0 +1,19 @@
+package uk.ac.osswatch.simal.model;
+
+import uk.ac.osswatch.simal.model.elmo.ProjectException;
+
+/**
+ * An interface for wrapping repository specific representations of
+ * a Project. Other classes should not access the repository classes
+ * directly, instead they should access the data through a class that 
+ * implements this interface.
+ *
+ * @see uk.ac.osswatch.simal.model.elmo.Project
+ */
+public interface IProject {	
+  public String getName() throws ProjectException;
+  public void setName(String name) throws ProjectException;
+  
+  public String getShortDesc() throws ProjectException;
+  public void setShortDesc(String shortDesc) throws ProjectException;
+}
---
+++ trunk/uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/model/elmo/ProjectException.java
@@ -0,0 +1,9 @@
+package uk.ac.osswatch.simal.model.elmo;
+
+public class ProjectException extends Exception {
+	private static final long serialVersionUID = -952255983346023938L;
+
+	public ProjectException(String msg) {
+		super(msg);
+	}
+}
