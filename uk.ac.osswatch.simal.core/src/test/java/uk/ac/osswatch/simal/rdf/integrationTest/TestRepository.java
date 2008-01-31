9feb1d5157047dfaea660ede9806cfe2b29c1aa1
rgardler@apache.org <rgardler@apache.org@cad361de-782e-0410-9987-355e4c56c873>
2008-01-31 23:14:21 +0000

    @refactor Use static import for Assert

--- uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/rdf/integrationTest/TestRepository.java
+++ uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/rdf/integrationTest/TestRepository.java
@@ -1,11 +1,12 @@
 package uk.ac.osswatch.simal.rdf.integrationTest;
 
 import java.io.StringWriter;
+import java.util.Iterator;
 import java.util.Set;
 
 import javax.xml.namespace.QName;
 
-import junit.framework.Assert;
+import static junit.framework.Assert.*;
 
 import org.junit.Test;
 
@@ -23,43 +24,43 @@ public class TestRepository extends BaseRepositoryTest {
 	public void testAddProject() throws SimalRepositoryException {
 		SimalRepository repo = getTestRepo();
 		// The default test repository adds projects when it is instantiated
-		Assert.assertNotNull(repo);
+		assertNotNull(repo);
 	}
 
 	@Test
 	public void testFindProject() throws SimalRepositoryException {
 		SimalRepository repo = getTestRepo(true);
-		
+
 		QName qname = new QName("http://foo.org/nonExistent");
 		try {
-		Project project = repo.getProject(qname);
-		Assert.assertNull(project);
+			Project project = repo.getProject(qname);
+			assertNull(project);
 
-		// test a known valid file
-		project = getSimalTestProject(true);
-		Assert.assertEquals("Simal DOAP Test",
-				project.getName());
+			// test a known valid file
+			project = getSimalTestProject(true);
+			assertEquals("Simal DOAP Test", project.getName());
 		} finally {
 			repo.close();
-		}		
+		}
 	}
 
 	@Test
 	public void testGetRdfXml() throws SimalRepositoryException {
 		SimalRepository repo = getTestRepo();
 		QName qname = new QName(TEST_SIMAL_QNAME);
-		
+
 		StringWriter sw = new StringWriter();
 		repo.writeXML(sw, qname);
 		String xml = sw.toString();
-		Assert.assertTrue("XML does not contain the QName expected", xml.contains("rdf:about=\"http://simal.oss-watch.ac.uk/simalTest#\""));
+		assertTrue("XML does not contain the QName expected",
+					xml.contains("rdf:about=\"http://simal.oss-watch.ac.uk/simalTest#\""));
 	}
-	
+
 	@Test
 	public void testGetAllProjects() throws SimalRepositoryException {
 		SimalRepository repo = getTestRepo();
-		
+
 		Set<Project> projects = repo.getAllProjects();
-		Assert.assertEquals(4, projects.size());
+		assertEquals(4, projects.size());
 	}
 }
