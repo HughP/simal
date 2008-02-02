1744c82289c941d8925479e7cf8181f8cbf6c463
rgardler@apache.org <rgardler@apache.org@cad361de-782e-0410-9987-355e4c56c873>
2008-02-02 00:22:52 +0000

    - Add a link to the details page
    - @refactor tidy up the population of the panel

--- uk.ac.osswatch.simal.web/src/test/java/uk/ac/osswatch/simal/wicket/panel/TestProjectSummaryPanel.java
+++ uk.ac.osswatch.simal.web/src/test/java/uk/ac/osswatch/simal/wicket/panel/TestProjectSummaryPanel.java
@@ -1,12 +1,13 @@
 package uk.ac.osswatch.simal.wicket.panel;
 
-import javax.xml.namespace.QName;
-
 import junit.framework.TestCase;
 
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.util.tester.TestPanelSource;
 import org.apache.wicket.util.tester.WicketTester;
+
+import uk.ac.osswatch.simal.wicket.ProjectDetailPage;
+import uk.ac.osswatch.simal.wicket.UserApplication;
 
 /**
  * Simple test using the WicketTester
@@ -20,15 +21,16 @@ public class TestProjectSummaryPanel extends TestCase {
 
 	@SuppressWarnings("serial")
 	public void testRenderUserHomePage() {
-		 tester.startPanel(new TestPanelSource()
-		 {
+		 tester.startPanel(new TestPanelSource() {
 		        public Panel getTestPanel(String panelId)
 		        {
-		                QName qname = new QName("http://simal.oss-watch.ac.uk/simalTest#");
-		                return new ProjectSummaryPanel(panelId, qname);
+		                return new ProjectSummaryPanel(panelId, UserApplication.getProject(UserApplication.DEFAULT_PROJECT_QNAME));
 		        }
 		 });
 		tester.assertVisible("panel:projectName");
 		tester.assertLabel("panel:projectName", "Simal DOAP Test");
+		
+		tester.clickLink("panel:projectDetailLink");
+		tester.assertRenderedPage(ProjectDetailPage.class);
 	}
 }
