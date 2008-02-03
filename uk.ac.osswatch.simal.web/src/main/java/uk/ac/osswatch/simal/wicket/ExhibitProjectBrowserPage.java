9ed2ddf8a9d10a50f2c627cde91c5bb48de960e2
rgardler@apache.org <rgardler@apache.org@cad361de-782e-0410-9987-355e4c56c873>
2008-02-03 21:15:43 +0000

    Add a basic Exhibit browser for projects

---
+++ uk.ac.osswatch.simal.web/src/main/java/uk/ac/osswatch/simal/wicket/ExhibitProjectBrowserPage.java
@@ -0,0 +1,38 @@
+package uk.ac.osswatch.simal.wicket;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.net.URL;
+
+import org.apache.wicket.behavior.HeaderContributor;
+import org.apache.wicket.behavior.StringHeaderContributor;
+import org.apache.wicket.markup.html.resources.CompressedResourceReference;
+
+/**
+ * Creates a page which contains an <a
+ * href="http://simile.mit.edu/wiki/Exhibit">Exhibit 2.0</a> Browser. This is a
+ * faceted browser for Projects.
+ */
+public class ExhibitProjectBrowserPage extends BasePage {
+	private static final long serialVersionUID = 2675836864409849552L;
+	private static final CompressedResourceReference EXHIBIT_CSS = new CompressedResourceReference(
+			UserApplication.class, "exhibit.css");
+
+	public ExhibitProjectBrowserPage() {
+		URL dir = UserApplication.class.getResource("default.css");
+		try {
+			File outFile = new File(new File(dir.toURI()).getParent() + File.separator + "projects.js");
+			FileWriter out = new FileWriter(outFile);
+			out.write(UserApplication.getRepository().getAllProjectsAsJSON());
+			out.close();
+		} catch (Exception e) {
+			UserReportableException error = new UserReportableException("Unable to write JSON file", ExhibitProjectBrowserPage.class, e);
+			setResponsePage(new ErrorReportPage(error));
+		}
+		add(HeaderContributor.forCss(EXHIBIT_CSS));
+		add(HeaderContributor
+				.forJavaScript("http://static.simile.mit.edu/exhibit/api-2.0/exhibit-api.js"));
+		add(new StringHeaderContributor(
+				"<link href=\"/simal-webapp/resources/uk.ac.osswatch.simal.wicket.UserApplication/projects.js\" type=\"application/json\" rel=\"exhibit/data\" />"));
+	}
+}
