78d29277a0c4ca131a127539d81b71581f6ef267
rgardler@apache.org <rgardler@apache.org@cad361de-782e-0410-9987-355e4c56c873>
2008-02-03 22:06:16 +0000

    @refactor to match the class structure of Elmo

--- uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/model/elmo/Project.java
+++ uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/model/elmo/Project.java
@@ -1,8 +1,6 @@
 package uk.ac.osswatch.simal.model.elmo;
 
-import java.util.Set;
 
-import javax.xml.namespace.QName;
 
 import uk.ac.osswatch.simal.model.IProject;
 
@@ -11,9 +9,8 @@ import uk.ac.osswatch.simal.model.IProject;
  * 
  * @see org.openrdf.concepts.doap.Project
  */
-public class Project implements IProject {
-	private static final long serialVersionUID = -7610178891247360114L;
-	private org.openrdf.concepts.doap.Project elmoProject;
+public class Project extends DoapResource implements IProject {
+	private static final long serialVersionUID = -1771017230656089944L;
 
 	/**
 	 * Create a new wrapper around an elmo Project object.
@@ -22,67 +19,6 @@ public class Project implements IProject {
 	 */
 	public Project(org.openrdf.concepts.doap.Project elmoProject) {
 		this.elmoProject = elmoProject;
-	}
-
-	/**
-	 * Returns the first name in the set of names contained within the Elmo
-	 * Project class.
-	 */
-	public String getName() {
-		return (String) elmoProject.getDoapNames().toArray()[0];
-	}
-
-	public String getShortDesc() {
-		return elmoProject.getDoapShortdesc();
-	}
-
-	/**
-	 * Sets the name of the project. Note that this assumes that there is only
-	 * one name. In the event of there being more than one name it throws an
-	 * exception.
-	 * 
-	 * @throws ProjectException
-	 *             if the project has more than one name
-	 */
-	public void setName(String name) throws ProjectException {
-		if (elmoProject.getDoapNames() != null) {
-			if (elmoProject.getDoapNames().size() != 1) {
-				throw new ProjectException(
-						"At present the Project wrapper can only handle a single project name.");
-			}
-			Set<Object> names = elmoProject.getDoapNames();
-			names.remove(names.toArray()[0]);
-			names.add(name);
-		}
-	}
-
-	public void setShortDesc(String shortDesc) {
-		elmoProject.setDoapShortdesc(shortDesc);
-	}
-
-	public QName getQName() {
-		return elmoProject.getQName();
-	}
-
-	public String toString() {
-		return getName();
-	}
-
-	public String toJSON(boolean asRecord) {
-		StringBuffer json = new StringBuffer();
-		if (!asRecord) {
-			json.append("{ \"items\": [");
-		}
-		json.append("{");
-		json.append("\"id\":\"" + getQName().getNamespaceURI() + "\",");
-		json.append("\"label\":\"" + getName() + "\",");
-		json.append("\"name\":\"" + getName() + "\",");
-		json.append("\"shortdesc\":\"" + getShortDesc() + "\"");
-		json.append("}");
-		if (!asRecord) {
-			json.append("]}");
-		}
-		return json.toString();
 	}
 
 }
