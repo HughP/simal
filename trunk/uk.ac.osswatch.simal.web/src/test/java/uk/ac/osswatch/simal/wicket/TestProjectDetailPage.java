976c847c7ed85af0ce66f266355815ee5c7baca0
rgardler@apache.org <rgardler@apache.org@cad361de-782e-0410-9987-355e4c56c873>
2008-02-08 22:46:45 +0000

    Display full DOAP info (all info currently in its raw form, i.e. strings)

--- trunk/uk.ac.osswatch.simal.web/src/test/java/uk/ac/osswatch/simal/wicket/TestProjectDetailPage.java
+++ trunk/uk.ac.osswatch.simal.web/src/test/java/uk/ac/osswatch/simal/wicket/TestProjectDetailPage.java
@@ -1,7 +1,5 @@
 package uk.ac.osswatch.simal.wicket;
 
-import org.apache.wicket.util.tester.WicketTester;
-
 /**
  * Simple test using the WicketTester
  */
@@ -10,8 +8,12 @@ public class TestProjectDetailPage extends TestBase {
 	public void testRenderPage() {
 		tester.startPage(ProjectDetailPage.class);
 		tester.assertRenderedPage(ProjectDetailPage.class);
+		
 		tester.assertVisible("projectName");
 		tester.assertVisible("shortDesc");
+		tester.assertVisible("created");
+		tester.assertVisible("description");
+		
 		tester.assertVisible("footer");
 	}
 }
