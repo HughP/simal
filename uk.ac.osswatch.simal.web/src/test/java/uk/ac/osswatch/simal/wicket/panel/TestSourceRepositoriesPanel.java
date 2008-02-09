36c201c7e0d6d5f934ed3aa16d5a2b711a81ebbf
rgardler@apache.org <rgardler@apache.org@cad361de-782e-0410-9987-355e4c56c873>
2008-02-09 23:17:17 +0000

    Add reasonable rendering of repositories

---
+++ uk.ac.osswatch.simal.web/src/test/java/uk/ac/osswatch/simal/wicket/panel/TestSourceRepositoriesPanel.java
@@ -0,0 +1,34 @@
+package uk.ac.osswatch.simal.wicket.panel;
+
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.util.tester.TestPanelSource;
+
+import uk.ac.osswatch.simal.rdf.SimalRepository;
+import uk.ac.osswatch.simal.rdf.SimalRepositoryException;
+import uk.ac.osswatch.simal.wicket.TestBase;
+import uk.ac.osswatch.simal.wicket.UserApplication;
+
+/**
+ * Simple test using the WicketTester
+ */
+public class TestSourceRepositoriesPanel extends TestBase {
+
+	@SuppressWarnings("serial")
+	public void testRenderPanel() {
+		 tester.startPanel(new TestPanelSource() {
+		        public Panel getTestPanel(String panelId)
+		        {
+		                try {
+							return new SourceRepositoriesPanel(panelId, SimalRepository.getProject(UserApplication.DEFAULT_PROJECT_QNAME).getRepositories());
+						} catch (SimalRepositoryException e) {
+							fail(e.getMessage());
+							return null;
+						}
+		        }
+		 });
+		tester.assertVisible("panel:sourceRepositories");
+		tester.assertVisible("panel:sourceRepositories:1:anonRoots");
+		tester.assertVisible("panel:sourceRepositories:1:devLocations:1:devLink");
+		tester.assertVisible("panel:sourceRepositories:1:browseRoots:1:browseLink");
+	}
+}
