e2cffa92db99242307185d073234509379612f6b
rgardler@apache.org <rgardler@apache.org@cad361de-782e-0410-9987-355e4c56c873>
2008-02-10 21:43:33 +0000

    Add a personSummaryPanel and use it in the Project Detail page

---
+++ trunk/uk.ac.osswatch.simal.web/src/test/java/uk/ac/osswatch/simal/wicket/panel/TestPersonSummaryPanel.java
@@ -0,0 +1,35 @@
+package uk.ac.osswatch.simal.wicket.panel;
+
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.util.tester.TestPanelSource;
+
+import uk.ac.osswatch.simal.model.elmo.Person;
+import uk.ac.osswatch.simal.rdf.SimalRepository;
+import uk.ac.osswatch.simal.rdf.SimalRepositoryException;
+import uk.ac.osswatch.simal.wicket.TestBase;
+import uk.ac.osswatch.simal.wicket.UserApplication;
+
+/**
+ * Simple test using the WicketTester
+ */
+public class TestPersonSummaryPanel extends TestBase {
+
+	@SuppressWarnings("serial")
+	public void testRenderUserHomePage() {
+		 tester.startPanel(new TestPanelSource() {
+		        public Panel getTestPanel(String panelId)
+		        {
+		                try {
+							return new PersonSummaryPanel(panelId, (Person)SimalRepository.getProject(UserApplication.DEFAULT_PROJECT_QNAME).getDevelopers().toArray()[0]);
+						} catch (SimalRepositoryException e) {
+							fail(e.getMessage());
+							return null;
+						}
+		        }
+		 });
+		tester.assertVisible("panel:personName");
+		tester.assertLabel("panel:personName", "developer");
+		
+		tester.assertVisible("panel:homepages");
+	}
+}
