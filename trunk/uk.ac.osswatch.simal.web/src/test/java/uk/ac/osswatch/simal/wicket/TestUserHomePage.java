ce81ba16e968acecfaf7fee48145a0a9b5e6326a
rgardler@apache.org <rgardler@apache.org@cad361de-782e-0410-9987-355e4c56c873>
2008-02-12 23:13:10 +0000

    message has now been removed

--- trunk/uk.ac.osswatch.simal.web/src/test/java/uk/ac/osswatch/simal/wicket/TestUserHomePage.java
+++ trunk/uk.ac.osswatch.simal.web/src/test/java/uk/ac/osswatch/simal/wicket/TestUserHomePage.java
@@ -1,7 +1,5 @@
 package uk.ac.osswatch.simal.wicket;
 
-import org.apache.wicket.util.tester.WicketTester;
-
 /**
  * Simple test using the WicketTester
  */
@@ -10,7 +8,6 @@ public class TestUserHomePage extends TestBase {
 	public void testRenderPage() {
 		tester.startPage(UserHomePage.class);
 		tester.assertRenderedPage(UserHomePage.class);
-		tester.assertVisible("message");
 		tester.assertVisible("projectList");
 		tester.assertVisible("featuredProject");
 		tester.assertVisible("footer");
