aea6f89f82ed7ae6173995fd2c2999089c164754
rgardler@apache.org <rgardler@apache.org@cad361de-782e-0410-9987-355e4c56c873>
2008-02-12 23:08:05 +0000

    - Don't throw away blank nodes (i.e. badly formed DOAP), instead use a default namespace
    - convert foaf:name to foaf:givenname (elmo doesn't support foaf:name for some reason)

--- uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/rdf/SimalRepository.java
+++ uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/rdf/SimalRepository.java
@@ -1,5 +1,6 @@
 package uk.ac.osswatch.simal.rdf;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Writer;
 import java.net.URL;
@@ -36,19 +37,22 @@ import uk.ac.osswatch.simal.model.elmo.Version;
 import uk.ac.osswatch.simal.rdf.io.AnnotatingRDFXMLHandler;
 
 /**
- * A class for handling common repository actions. Applications should
- * not instantiate this class but should interact with it via its
- * static methods.
+ * A class for handling common repository actions. Applications should not
+ * instantiate this class but should interact with it via its static methods.
  * 
  */
 public class SimalRepository {
+	public static final String TEST_FILE_BASE_URL = "http://exmple.org/baseURI";
+	public static final String TEST_FILE_URI_NO_QNAME = "testNoRDFAboutDOAP.xml";
+	public static final String DEFAULT_NAMESPACE_URI = "http://simal/oss-watch.ac.uk/defaultNS#";
 	private static SailRepository _repository;
-	
+
 	private SimalRepository() {
 		super();
 	}
 
-	private static RepositoryConnection getConnection() throws SimalRepositoryException {
+	private static RepositoryConnection getConnection()
+			throws SimalRepositoryException {
 		return getManager().getConnection();
 	}
 
@@ -64,27 +68,24 @@ public class SimalRepository {
 	public static void addProject(URL url, String baseURI)
 			throws SimalRepositoryException {
 		verifyInitialised();
-		
+
 		RDFParser parser = new RDFXMLParser();
-		parser.setRDFHandler(new AnnotatingRDFXMLHandler());
+		// FIXME: use a proper file location
+		File annotatedFile;
+		try {
+			annotatedFile = new File("annotatedRDFXML.xml");
+			parser.setRDFHandler(new AnnotatingRDFXMLHandler(
+					annotatedFile));
+		} catch (IOException e) {
+			throw new SimalRepositoryException(
+					"Unable to write the annotated RDF/XML file", e);
+		}
 		parser.setVerifyData(true);
 
 		RepositoryConnection con = getConnection();
 		try {
-			try {
-				parser.parse(url.openStream(), baseURI);
-			} catch (RDFHandlerException e) {
-				if (e.getMessage().equals(
-						AnnotatingRDFXMLHandler.NO_QNAME_PRESENT)) {
-					throw new SimalRepositoryException(
-							"The RDF/XML representation does not appear to have a QName, Simal will not handle anonymous projects",
-							e);
-				} else {
-					throw new SimalRepositoryException(
-							"Unable to access the repository", e);
-				}
-			}
-			con.add(url, baseURI, RDFFormat.RDFXML);
+			parser.parse(url.openStream(), baseURI);
+			con.add(annotatedFile, baseURI, RDFFormat.RDFXML);
 		} catch (RDFParseException e) {
 			throw new SimalRepositoryException(
 					"Attempt to add unparseable RDF/XML to the repository", e);
@@ -94,9 +95,12 @@ public class SimalRepository {
 		} catch (IOException e) {
 			throw new SimalRepositoryException(
 					"Unable to read the RDF/XML file", e);
+		} catch (RDFHandlerException e) {
+			throw new SimalRepositoryException("Problem handling RDF data", e);
 		} finally {
 			try {
 				con.close();
+				annotatedFile.delete();
 			} catch (RepositoryException e) {
 				throw new SimalRepositoryException(
 						"Unable to close the connection", e);
@@ -105,14 +109,15 @@ public class SimalRepository {
 	}
 
 	/**
-	 * Checks to see if the repository has been correctly initialised.
-	 * If it has not then an exception is thrown.
+	 * Checks to see if the repository has been correctly initialised. If it has
+	 * not then an exception is thrown.
 	 * 
 	 * @throws SimalRepositoryException
 	 */
 	private static void verifyInitialised() throws SimalRepositoryException {
 		if (!isInitialised()) {
-			throw new SimalRepositoryException("SimalRepsotory has not been initialised. Call one of the initialise methods first.");
+			throw new SimalRepositoryException(
+					"SimalRepsotory has not been initialised. Call one of the initialise methods first.");
 		}
 	}
 
@@ -120,7 +125,7 @@ public class SimalRepository {
 	 * Get a manager for concepts defined by Elmo.
 	 * 
 	 * @return
-	 * @throws SimalRepositoryException 
+	 * @throws SimalRepositoryException
 	 */
 	private static SesameManager getManager() throws SimalRepositoryException {
 		verifyInitialised();
@@ -141,11 +146,12 @@ public class SimalRepository {
 	 * @param qname
 	 *            the QName of the project to retrieve
 	 * @return the project, or if no project with the given QName exists Null
-	 * @throws SimalRepositoryException 
+	 * @throws SimalRepositoryException
 	 */
-	public static Project getProject(QName qname) throws SimalRepositoryException {
+	public static Project getProject(QName qname)
+			throws SimalRepositoryException {
 		verifyInitialised();
-		
+
 		org.openrdf.concepts.doap.Project elmoProject = getManager().find(
 				org.openrdf.concepts.doap.Project.class, qname);
 		if (elmoProject == null) {
@@ -153,18 +159,20 @@ public class SimalRepository {
 		}
 		return new Project(elmoProject);
 	}
-	
+
 	/**
 	 * Get a release version from the repository.
 	 * 
 	 * @param qname
 	 *            the QName of the release version to retrieve
-	 * @return the release version, or if no release version with the given QName exists Null
-	 * @throws SimalRepositoryException 
+	 * @return the release version, or if no release version with the given
+	 *         QName exists Null
+	 * @throws SimalRepositoryException
 	 */
-	public static IVersion getVersion(QName qname) throws SimalRepositoryException {
+	public static IVersion getVersion(QName qname)
+			throws SimalRepositoryException {
 		verifyInitialised();
-		
+
 		org.openrdf.concepts.doap.Version elmoVersion = getManager().find(
 				org.openrdf.concepts.doap.Version.class, qname);
 		if (elmoVersion == null) {
@@ -172,19 +180,20 @@ public class SimalRepository {
 		}
 		return new Version(elmoVersion);
 	}
-	
+
 	/**
 	 * Get a version control repository from the repository.
 	 * 
 	 * @param qname
 	 *            the QName of the repository to retrieve
-	 * @return the repository or if no repository with the given QName exists Null
-	 * @throws SimalRepositoryException 
-	 * @throws SimalRepositoryException 
+	 * @return the repository or if no repository with the given QName exists
+	 *         Null
+	 * @throws SimalRepositoryException
+	 * @throws SimalRepositoryException
 	 */
 	public static IRCS getRCS(QName qname) throws SimalRepositoryException {
 		verifyInitialised();
-		
+
 		org.openrdf.concepts.doap.Repository elmoRepo = getManager().find(
 				org.openrdf.concepts.doap.Repository.class, qname);
 		if (elmoRepo == null) {
@@ -193,18 +202,19 @@ public class SimalRepository {
 		return new RCS(elmoRepo);
 	}
 
-	
 	/**
 	 * Get a person from the repository.
 	 * 
 	 * @param qname
 	 *            the QName of the repository to retrieve
-	 * @return the repository or if no repository with the given QName exists Null
-	 * @throws SimalRepositoryException 
+	 * @return the repository or if no repository with the given QName exists
+	 *         Null
+	 * @throws SimalRepositoryException
 	 */
-	public static IPerson getPerson(QName qname) throws SimalRepositoryException {
+	public static IPerson getPerson(QName qname)
+			throws SimalRepositoryException {
 		verifyInitialised();
-		
+
 		org.openrdf.concepts.foaf.Person elmoPerson = getManager().find(
 				org.openrdf.concepts.foaf.Person.class, qname);
 		if (elmoPerson == null) {
@@ -215,7 +225,7 @@ public class SimalRepository {
 
 	public static Set<Project> getAllProjects() throws SimalRepositoryException {
 		verifyInitialised();
-		
+
 		HashSet<Project> result = new HashSet<Project>();
 		Iterator<org.openrdf.concepts.doap.Project> elmoProjects = getManager()
 				.findAll(org.openrdf.concepts.doap.Project.class).iterator();
@@ -240,7 +250,7 @@ public class SimalRepository {
 	public static void writeXML(Writer writer, QName qname)
 			throws SimalRepositoryException {
 		verifyInitialised();
-		
+
 		getProject(qname);
 		RDFXMLPrettyWriter XMLWriter = new RDFXMLPrettyWriter(writer);
 		try {
@@ -258,17 +268,17 @@ public class SimalRepository {
 	/**
 	 * Adds test data to the repo. be careful to only use this when the repo in
 	 * use is a test repository.
-	 * @throws SimalRepositoryException 
+	 * 
+	 * @throws SimalRepositoryException
 	 * 
 	 * @throws SimalRepositoryException
 	 */
 	private static void addTestData() throws SimalRepositoryException {
 		verifyInitialised();
-		
+
 		try {
 			addProject(SimalRepository.class
-					.getResource("testNoRDFAboutDOAP.xml"),
-					"http://exmple.org/baseURI");
+					.getResource(TEST_FILE_URI_NO_QNAME), TEST_FILE_BASE_URL);
 		} catch (SimalRepositoryException e2) {
 			// Yes, we expected that
 			// This is here so that the relevant test will fail
@@ -278,10 +288,10 @@ public class SimalRepository {
 		// Local test files
 		try {
 			addProject(SimalRepository.class.getResource("testDOAP.xml"),
-					"http://exmple.org/baseURI");
+					TEST_FILE_BASE_URL);
 
 			addProject(SimalRepository.class.getResource("ossWatchDOAP.xml"),
-					"http://exmple.org/baseURI");
+					TEST_FILE_BASE_URL);
 
 			addProject(new URL(
 					"http://simal.oss-watch.ac.uk/projectDetails/codegoo.rdf"),
@@ -319,11 +329,11 @@ public class SimalRepository {
 	}
 
 	/**
-	 * Get all the projects in the repository and return them in a
-	 * single JSON file.
+	 * Get all the projects in the repository and return them in a single JSON
+	 * file.
 	 * 
 	 * @return
-	 * @throws SimalRepositoryException 
+	 * @throws SimalRepositoryException
 	 */
 	public static String getAllProjectsAsJSON() throws SimalRepositoryException {
 		StringBuffer json = new StringBuffer("{ \"items\": [");
@@ -339,8 +349,8 @@ public class SimalRepository {
 	}
 
 	/**
-	 * Return true if this repository has been successfully initialised
-	 * and is ready to be used, otherwise return false.
+	 * Return true if this repository has been successfully initialised and is
+	 * ready to be used, otherwise return false.
 	 * 
 	 * @return
 	 */
@@ -350,33 +360,37 @@ public class SimalRepository {
 
 	/**
 	 * Shutdown the repository cleanly.
-	 * @throws SimalRepositoryException 
-     *
+	 * 
+	 * @throws SimalRepositoryException
+	 * 
 	 * @throws RepositoryException
 	 */
 	public static void destroy() throws SimalRepositoryException {
 		verifyInitialised();
-		
+
 		try {
 			_repository.shutDown();
 			_repository = null;
 		} catch (RepositoryException e) {
-			throw new SimalRepositoryException("Unable to shutdown the repository, refusing to destroy it.");
+			throw new SimalRepositoryException(
+					"Unable to shutdown the repository, refusing to destroy it.");
 		}
 	}
 
 	/**
-	 * Initialise a default repository. Currently this creates a volatile repository
-	 * populated with test data.
-	 * @throws SimalRepositoryException 
+	 * Initialise a default repository. Currently this creates a volatile
+	 * repository populated with test data.
+	 * 
+	 * @throws SimalRepositoryException
 	 * 
 	 * @throws SimalRepositoryException
 	 */
 	public static void initialise() throws SimalRepositoryException {
 		if (_repository != null) {
-			throw new SimalRepositoryException("Illegal attempt to create a second SimalRepository in the same JAVA VM.");
+			throw new SimalRepositoryException(
+					"Illegal attempt to create a second SimalRepository in the same JAVA VM.");
 		}
-		
+
 		_repository = new SailRepository(new MemoryStore());
 		try {
 			_repository.initialize();
@@ -385,6 +399,6 @@ public class SimalRepository {
 					"Unable to intialise the repository", e);
 		}
 
-		addTestData();		
+		addTestData();
 	}
 }
--- uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/rdf/io/AnnotatingRDFXMLHandler.java
+++ uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/rdf/io/AnnotatingRDFXMLHandler.java
@@ -1,43 +1,129 @@
 package uk.ac.osswatch.simal.rdf.io;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.openrdf.model.Resource;
 import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.model.impl.LiteralImpl;
+import org.openrdf.model.impl.StatementImpl;
+import org.openrdf.model.impl.URIImpl;
 import org.openrdf.rio.RDFHandler;
 import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.rdfxml.RDFXMLWriter;
+
+import uk.ac.osswatch.simal.rdf.SimalRepository;
 
 /**
- * Validates and annotates an RDF/XML file for use within SIMAL.
- * This handler does things like ensure a valid QName is present
- * and key Simal information is captured.
- *  
+ * Validates and annotates an RDF/XML file for use within SIMAL. This handler
+ * does things like ensure a valid QName is present and key Simal information is
+ * captured. <br/>
+ * <h3>FIXME</h3>
+ * At present this handler wraps an RDFXMLWriter and writes the file to the
+ * local disk. This can then be read from the disk complete with annotations.
+ * This is unwanted overhead, ideally this handler will be re-written to
+ * annotate the file on the fly as it is being added to the repository.
+ * 
  */
 public class AnnotatingRDFXMLHandler implements RDFHandler {
 
-	public static final String NO_QNAME_PRESENT = "No valid QName present";
+	private RDFXMLWriter handler;
+
+	/**
+	 * Create a new AnnotatingRDFXMLHandler that will write to a file.
+	 * 
+	 * @param file
+	 *            the file to write the annotated RDF/XML output to.
+	 * @throws IOException
+	 *             if there is a problem creating the output file
+	 */
+	public AnnotatingRDFXMLHandler(File file) throws IOException {
+		handler = new RDFXMLWriter(new FileWriter(file));
+	}
 
 	public void endRDF() throws RDFHandlerException {
-		
+		handler.endRDF();
 	}
 
-	public void handleComment(String arg0) throws RDFHandlerException {
-		
+	public void handleComment(String comment) throws RDFHandlerException {
+		handler.handleComment(comment);
 	}
 
-	public void handleNamespace(String arg0, String arg1)
+	public void handleNamespace(String prefix, String name)
 			throws RDFHandlerException {
-		
+		handler.handleNamespace(prefix, name);
 	}
 
-	public void handleStatement(Statement statement) throws RDFHandlerException {
-		String subject = statement.getSubject().toString();
-		if (subject.startsWith("_:") || subject.indexOf(",") > 0 || subject.lastIndexOf("/") == subject.length()) {
-			// looks like we have no QName
-			throw new RDFHandlerException(NO_QNAME_PRESENT);
+	public void handleStatement(Statement inStatement)
+			throws RDFHandlerException {
+		Statement outStatement = inStatement;
+		Resource outSubject;
+		URI outPredicate;
+		Value outValue;
+
+		outSubject = verifyQName(inStatement.getSubject());
+		outValue = fixEncoding(inStatement.getObject());
+		outPredicate = fixFoaf(inStatement.getPredicate());
+
+		outStatement = new StatementImpl(outSubject,
+				outPredicate, outValue);
+
+		handler.handleStatement(outStatement);
+	}
+
+	/**
+	 * Fixes up statements in the FOAF schema.
+	 * 
+	 * @param predicate
+	 * @return
+	 */
+	private URI fixFoaf(final URI inPredicate) {
+		URI outPredicate = inPredicate;
+		String strValue = inPredicate.stringValue();
+		if (strValue.contains("/foaf/")) {
+			if (strValue.endsWith("/name")) {
+			  // for some reason elmo does not support foaf:name, for now just convert to givenname
+			  outPredicate = new URIImpl(strValue.substring(0, strValue.length() - 5) + "/givenname");
+			}
 		}
-		//System.out.println(statement.toString());
+		return outPredicate;
+	}
+
+	private Value fixEncoding(final Value inValue)
+			throws RDFHandlerException {
+		Value outValue = inValue;
+		
+		if (inValue instanceof LiteralImpl) {
+			String strValue = inValue.stringValue();
+			try {
+				byte[] utf8Bytes = strValue.getBytes("UTF8");
+				outValue = new LiteralImpl(new String(utf8Bytes));
+			} catch (UnsupportedEncodingException e) {
+				throw new RDFHandlerException("Unable to encode value", e);
+			}
+		}
+		return outValue;
+	}
+
+	private Resource verifyQName(final Resource inSubject) {
+		Resource outSubject = inSubject;
+
+		String strSubject = inSubject.toString();
+		if (strSubject.startsWith("_:") || strSubject.indexOf(",") > 0
+				|| strSubject.lastIndexOf("/") == strSubject.length()) {
+			// looks like we have no QName
+			outSubject = new URIImpl(SimalRepository.DEFAULT_NAMESPACE_URI
+					+ inSubject);
+		}
+		return outSubject;
 	}
 
 	public void startRDF() throws RDFHandlerException {
-		
+		handler.startRDF();
 	}
 
 }
