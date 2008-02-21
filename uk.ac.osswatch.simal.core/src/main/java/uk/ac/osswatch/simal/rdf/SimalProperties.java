ac6422f9f1e29200e9025c075072e30989bce1e7
rgardler@apache.org <rgardler@apache.org@cad361de-782e-0410-9987-355e4c56c873>
2008-02-21 22:34:55 +0000

    Missed a couple of files for...
    Add properties system to the repository (ISSUE 74)
    
    @refactor we no longer have a largely static SimalRepository class (ISSUE 78)

---
+++ uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/rdf/SimalProperties.java
@@ -0,0 +1,51 @@
+package uk.ac.osswatch.simal.rdf;
+
+import java.net.URL;
+import java.util.Properties;
+
+/**
+ * For accessing the properties of a Simal Repository.
+ * 
+ */
+public class SimalProperties {
+	private static final String DEFAULT_PROPERTIES_FILE = "default.simal.properties";
+	public static final String PROPERTY_DATA_DIR = "simal.repository.dir";
+	public static final String PROPERTY_TEST = "simal.test";
+
+	private Properties props;
+
+	public SimalProperties() throws SimalRepositoryException {
+		initProperties();
+	}
+
+	/**
+	 * Initialise the properties by reading the default properties file.
+	 * 
+	 * @throws SimalRepositoryException
+	 */
+	public void initProperties() throws SimalRepositoryException {
+		props = new Properties();
+		URL defaultLocation = SimalRepository.class.getClassLoader().getResource(
+				DEFAULT_PROPERTIES_FILE);
+		if (defaultLocation == null) {
+			throw new SimalRepositoryException("Unable to find the default properties file");
+		}
+		try {
+			props.load(defaultLocation.openStream());
+		} catch (Exception e) {
+			throw new SimalRepositoryException(
+					"Unable to load default properties", e);
+		}
+	}
+
+	/**
+	 * Get a property value
+	 * 
+	 * @param property
+	 *            the name of the property value to retrieve
+	 */
+	public String getProperty(String property) {
+		return props.getProperty(property, "The property '" + property
+				+ "' has not been set");
+	}
+}
