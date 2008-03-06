e0d2ef3790acc6404323faf95b2bf78065c97332
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-03-06 23:22:27 +0000

    Ensure QNames are correctly set for foaf:Person and all doap:* nodes

--- uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/unitTest/model/io/TestAnnotatingRDFXMLHandler.java
+++ uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/unitTest/model/io/TestAnnotatingRDFXMLHandler.java
@@ -1,10 +1,14 @@
 package uk.ac.osswatch.simal.unitTest.model.io;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
 
 import org.junit.Test;
 import org.openrdf.rio.RDFHandlerException;
@@ -21,25 +25,33 @@ public class TestAnnotatingRDFXMLHandler {
 	public void testMissingQName() throws RDFParseException,
 			RDFHandlerException, IOException {
 		File annotatedFile = new File("testAnnotatedRDFXML.xml");
+    AnnotatingRDFXMLHandler annotatingHandler = new AnnotatingRDFXMLHandler(annotatedFile);
 		
 		RDFParser parser = new RDFXMLParser();
-		parser.setRDFHandler(new AnnotatingRDFXMLHandler(annotatedFile));
+    parser.setRDFHandler(annotatingHandler);
 		parser.setVerifyData(true);
 
 		parser.parse(SimalRepository.class.getResource(
 				SimalRepository.TEST_FILE_URI_NO_QNAME).openStream(),
 				SimalRepository.TEST_FILE_BASE_URL);
 		
-		// cool, it didn't throw an exception, but did it annotate the file?
+    Set<QName> qnames = annotatingHandler.getProjectQNames();
+    assertEquals("We don't seem to have collected the right number of project QNames", 1, qnames.size());
+    QName qname = qnames.iterator().next();
+    assertTrue("Generated project QName doesn't appear to be correct", qname.getLocalPart().startsWith(SimalRepository.DEFAULT_PROJECT_NAMESPACE_URI));
 		
 		Scanner scanner = new Scanner(annotatedFile);
 		String token;
-		boolean hasQName = false;
+		boolean hasProjectQName = false;
+                boolean hasPersonQName = false;
 		boolean hasFoafName = false;
         while (scanner.hasNext()) {
           token = scanner.next();
-          if (token.contains(SimalRepository.DEFAULT_NAMESPACE_URI)) {
-        	  hasQName = true;
+          if (token.contains(SimalRepository.DEFAULT_PROJECT_NAMESPACE_URI)) {
+        	  hasProjectQName = true;
+          }
+          if (token.contains(SimalRepository.DEFAULT_PERSON_NAMESPACE_URI)) {
+              hasPersonQName = true;
           }
           if (token.contains("foaf:name")) {
         	  hasFoafName = true;
@@ -49,9 +61,10 @@ public class TestAnnotatingRDFXMLHandler {
         
         // do the tests, if any of these file the annotated file will
         // be left on disk for inspection
-        assertTrue("Missing a QName", hasQName);
+        assertTrue("Missing a project QName", hasProjectQName);
+        assertTrue("Missing a person QName", hasPersonQName);
         assertTrue("foaf:name not converted to foaf:givenname", !hasFoafName);
         
         annotatedFile.delete();
-	}
+    }
 }
