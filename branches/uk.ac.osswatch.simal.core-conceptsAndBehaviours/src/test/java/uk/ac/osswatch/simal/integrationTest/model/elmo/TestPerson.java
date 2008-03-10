3fd3f03545eaf0de1e83c0f2c25f908f50ec5aab
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-03-10 21:13:56 +0000

    Moving towards concepts and behaviours

--- branches/uk.ac.osswatch.simal.core-conceptsAndBehaviours/src/test/java/uk/ac/osswatch/simal/integrationTest/model/elmo/TestPerson.java
+++ branches/uk.ac.osswatch.simal.core-conceptsAndBehaviours/src/test/java/uk/ac/osswatch/simal/integrationTest/model/elmo/TestPerson.java
@@ -8,36 +8,36 @@ import java.util.Set;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openrdf.concepts.foaf.Person;
 
 import uk.ac.osswatch.simal.integrationTest.rdf.BaseRepositoryTest;
-import uk.ac.osswatch.simal.model.IFaofPersonBehaviour;
 import uk.ac.osswatch.simal.model.elmo.FoafPersonBehaviour;
 import uk.ac.osswatch.simal.rdf.SimalRepositoryException;
 
 public class TestPerson extends BaseRepositoryTest {
 
-  private static Set<IFaofPersonBehaviour> maintainers;
-  private static Set<IFaofPersonBehaviour> developers;
-  private static Set<IFaofPersonBehaviour> documenters;
-  private static Set<IFaofPersonBehaviour> helpers;
-  private static Set<IFaofPersonBehaviour> translators;
-  private static Set<IFaofPersonBehaviour> testers;
-  private static FoafPersonBehaviour developer;
+  private static Set<Person> maintainers;
+  private static Set<Person> developers;
+  private static Set<Person> documenters;
+  private static Set<Person> helpers;
+  private static Set<Person> translators;
+  private static Set<Person> testers;
+  private static Person developer;
 
   @BeforeClass
   public static void setUpBeforeClass() throws Exception {
     createRepository();
 
     project1 = getSimalTestProject(false);
-    maintainers = project1.getMaintainers();
+    maintainers = project1.getDoapMaintainers();
 
-    developers = project1.getDevelopers();
-    developer = (FoafPersonBehaviour) developers.toArray()[0];
+    developers = project1.getDoapDevelopers();
+    developer = (Person)developers.toArray()[0];
 
-    helpers = project1.getHelpers();
-    documenters = project1.getHelpers();
-    translators = project1.getTranslators();
-    testers = project1.getTesters();
+    helpers = project1.getDoapHelpers();
+    documenters = project1.getDoapHelpers();
+    translators = project1.getDoapTranslators();
+    testers = project1.getDoapTesters();
   }
 
   @Test
@@ -52,34 +52,38 @@ public class TestPerson extends BaseRepositoryTest {
 
   @Test
   public void testNames() {
-    assertEquals("developer", developer.getGivennames());
+    assertEquals("developer", developer.getFoafGivennames().toString());
   }
 
   @Test
   public void testHomePage() {
     assertEquals("http://example.org/person/developer", developer
-        .getHomepages().toString());
+        .getFoafHomepages().toString());
   }
 
   @Test
   public void testToJSON() {
-    String json = developer.toJSON(false);
+    // FIXME: should not need to create behaviour manually
+    FoafPersonBehaviour behaviour = new FoafPersonBehaviour(developer);
+    String json = behaviour.toJSON(false);
     assertTrue("JSON does not include person name: JSON = " + json, json
-        .contains("\"label\":\"" + developer.getGivennames() + "\""));
+        .contains("\"label\":\"" + developer.getFoafGivennames() + "\""));
   }
 
   @Test
   public void testKnows() throws SimalRepositoryException {
-    Set<FoafPersonBehaviour> knows = developer.getKnows();
+    Set<Person> knows = developer.getFoafKnows();
     assertNotNull("Should know some people", knows);
-    FoafPersonBehaviour person = knows.iterator().next();
-    String givenName = person.getGivennames();
+    Person person = knows.iterator().next();
+    String givenName = (String)person.getFoafGivennames().toArray()[0];
     assertTrue("Should know Dan Brickley", givenName.contains("Dan Brickley"));
   }
 
   @Test
   public void testGetColleagues() throws SimalRepositoryException {
-    Set<IFaofPersonBehaviour> colleagues = developer.getColleagues();
+    // FIXME: should not need to create behaviour manually
+    FoafPersonBehaviour behaviour = new FoafPersonBehaviour(developer);
+    Set<Person> colleagues = behaviour.getColleagues();
     assertNotNull(colleagues);
     assertEquals("Got an incorrect number of colleagues", 6, colleagues.size());
   }
