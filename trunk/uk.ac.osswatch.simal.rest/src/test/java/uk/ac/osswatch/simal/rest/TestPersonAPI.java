00a294c3d65319db259e3fa28bf9ce71399c0023
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-03-16 00:16:26 +0000

    Test IDs of colleagues

--- trunk/uk.ac.osswatch.simal.rest/src/test/java/uk/ac/osswatch/simal/rest/TestPersonAPI.java
+++ trunk/uk.ac.osswatch.simal.rest/src/test/java/uk/ac/osswatch/simal/rest/TestPersonAPI.java
@@ -1,5 +1,6 @@
 package uk.ac.osswatch.simal.rest;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
@@ -12,5 +13,14 @@ public class TestPersonAPI extends AbstractAPITest {
     IAPIHandler handler = HandlerFactory.createHandler(command, repo);
     String result = handler.execute(command);
     assertNotNull(result);
+  }
+
+  @Test
+  public void testAllColleaguesXML() throws SimalAPIException {
+    String command = RESTServlet.COMMAND_ALL_COLLEAGUES + "xml";
+    IAPIHandler handler = HandlerFactory.createHandler(command, repo);
+    String result = handler.execute(command);
+    assertNotNull(result);
+    assertFalse("There should be no people with null IDs", result.contains("id=\"null\""));
   }
 }
