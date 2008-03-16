3731b0d9e58837f4c69befd27e70eadae040cc29
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-03-16 23:21:15 +0000

    It's still a bit hacky but (drum roll please) Simal is now a (bare bones) Open Social container...

--- trunk/uk.ac.osswatch.simal.web/src/main/java/uk/ac/osswatch/simal/wicket/UserHomePage.java
+++ trunk/uk.ac.osswatch.simal.web/src/main/java/uk/ac/osswatch/simal/wicket/UserHomePage.java
@@ -23,13 +23,15 @@ import uk.ac.osswatch.simal.wicket.panel.ProjectSummaryPanel;
 public class UserHomePage extends BasePage {
 	private static final long serialVersionUID = -8125606657250912738L;
 	private static final String MYEXPERIMENT_CLOUD_GADGET_URL = "http://hosting.gmodules.com/ig/gadgets/file/102930279039750035480/myexperiment-tag-cloud.xml";
+	private static final String FRIENDS_NAVIGATOR_GADGET_URL = "http://localhost:8080/gadgets/friendsNavigator.xml";
 
 	public UserHomePage() {
 		try {
 			add(new ProjectListPanel("projectList"));
 			add(new ProjectSummaryPanel("featuredProject", UserApplication
 					.getRepository().getProject(UserApplication.DEFAULT_PROJECT_QNAME)));
-			add(new GadgetPanel("myExperimentCloud", MYEXPERIMENT_CLOUD_GADGET_URL));
+			add(new GadgetPanel("friendsNavigatorGadget", FRIENDS_NAVIGATOR_GADGET_URL));
+			//add(new GadgetPanel("myExperimentCloud", MYEXPERIMENT_CLOUD_GADGET_URL));
 		} catch (SimalRepositoryException e) {
 			UserReportableException error = new UserReportableException("Unable to get project from the repository", ExhibitProjectBrowserPage.class, e);
 			setResponsePage(new ErrorReportPage(error));
