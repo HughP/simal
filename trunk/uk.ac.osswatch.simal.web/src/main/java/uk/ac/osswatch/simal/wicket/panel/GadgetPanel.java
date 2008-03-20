3709ee7695c458a4cbea9950c84d9a6869bd88e9
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-03-20 00:56:09 +0000

    Add an OpenSocialPage class for handling multple open social gadgets in a single page.
    Fixes ISSUE94

--- trunk/uk.ac.osswatch.simal.web/src/main/java/uk/ac/osswatch/simal/wicket/panel/GadgetPanel.java
+++ trunk/uk.ac.osswatch.simal.web/src/main/java/uk/ac/osswatch/simal/wicket/panel/GadgetPanel.java
@@ -1,37 +1,51 @@
 package uk.ac.osswatch.simal.wicket.panel;
 
-import org.apache.wicket.behavior.HeaderContributor;
-import org.apache.wicket.behavior.StringHeaderContributor;
+import java.rmi.server.UID;
+
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.markup.html.resources.CompressedResourceReference;
+import org.apache.wicket.model.Model;
 
 /**
  * A panel to display a Google Gadget (or Open Social gadget).
  */
 public class GadgetPanel extends Panel {
 	private static final long serialVersionUID = -5887625691251320087L;
-    private static final String SERVER_BASE = "http://localhost:8080/";
-    private static final String FILES_DIR = "container/";
-    private static final CompressedResourceReference GADGET_CSS = new CompressedResourceReference(GadgetPanel.class, "gadget.css");
-    
+  private UID uid;
+  private String specURL;
+   
+	
 	/**
 	 * Create a gadget panel.
 	 * 
-	 * @param id - the wicket ID for the panel
+	 * @param wicketID - the wicket ID for the panel
 	 * @param specURL - the specification of the gadget
 	 */
-	public GadgetPanel(String id, String specURL) {
-		super(id);
+	public GadgetPanel(String wicketID, String specURL) {
+		super(wicketID);
+		this.specURL = specURL;
+		uid = new UID();
 		
-		add(HeaderContributor.forCss(GADGET_CSS));
-		add(HeaderContributor
-				.forJavaScript(SERVER_BASE + "js/rpc.js?c=1"));
-		add(HeaderContributor
-				.forJavaScript(SERVER_BASE + FILES_DIR + "cookies.js"));
-		add(HeaderContributor
-				.forJavaScript(SERVER_BASE + FILES_DIR + "gadgets.js"));
-		add(new StringHeaderContributor("<script type=\"text/javascript\">var specUrl = '" + specURL + "'; var gadgetServerBase = '" + SERVER_BASE + "';</script>"));
+		AttributeAppender attr = new AttributeAppender("id", new Model(uid.toString()), " ");
+		WebMarkupContainer container = new WebMarkupContainer("gadget");
+		container.add(attr);
+		add(container);
 	}
+
+
+	/**
+	 * Get a unique identifier for this gadget.
+	 * @return
+	 */
+  public String getUid() {
+    return uid.toString();
+  }
+
+
+  public String getSpecURL() {
+    return specURL;
+  }
 
 }
 
