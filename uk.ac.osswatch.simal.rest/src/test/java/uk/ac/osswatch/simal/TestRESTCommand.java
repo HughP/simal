33123cda8ea094228c68317ac1606549f0ebf77f
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-03-24 23:14:05 +0000

    Allow a REST command to contain an alternative source for data (defaults to Simal)

--- uk.ac.osswatch.simal.rest/src/test/java/uk/ac/osswatch/simal/TestRESTCommand.java
+++ uk.ac.osswatch.simal.rest/src/test/java/uk/ac/osswatch/simal/TestRESTCommand.java
@@ -7,12 +7,22 @@ import org.junit.Test;
 public class TestRESTCommand {
 
   @Test
-  public void testCreateFromURI() {
+  public void testCreateDefaultSourceFromURI() {
     String cmdString = RESTCommand.COMMAND_ALL_COLLEAGUES + RESTCommand.PARAM_PERSON_ID + "1" + RESTCommand.FORMAT_JSON;
     RESTCommand cmd = RESTCommand.createCommand(cmdString);
     assertEquals("Command method is incorrect", RESTCommand.COMMAND_ALL_COLLEAGUES, cmd.getCommandMethod());
     assertEquals("Person ID is incorrect", "1", cmd.getPersonID());
     assertEquals("Format is incorrect", RESTCommand.FORMAT_JSON, cmd.getFormat());
     assertEquals("Source is incorrect", RESTCommand.SOURCE_TYPE_SIMAL, cmd.getSource());
+  }
+  
+  @Test
+  public void testCreateMyExperiementSOurceFromURI() {
+    String cmdString = RESTCommand.COMMAND_ALL_COLLEAGUES + RESTCommand.PARAM_PERSON_ID + "1" + RESTCommand.PARAM_SOURCE + RESTCommand.SOURCE_TYPE_MYEXPERIMENT + RESTCommand.FORMAT_JSON;
+    RESTCommand cmd = RESTCommand.createCommand(cmdString);
+    assertEquals("Source is incorrect", RESTCommand.SOURCE_TYPE_MYEXPERIMENT, cmd.getSource());
+    assertEquals("Command method is incorrect", RESTCommand.COMMAND_ALL_COLLEAGUES, cmd.getCommandMethod());
+    assertEquals("Person ID is incorrect", "1", cmd.getPersonID());
+    assertEquals("Format is incorrect", RESTCommand.FORMAT_JSON, cmd.getFormat());
   }
 }
