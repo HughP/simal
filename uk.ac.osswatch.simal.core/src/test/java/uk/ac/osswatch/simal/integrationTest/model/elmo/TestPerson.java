5bcf7b762dad41c22bbf936133c3d2d2ed4072ff
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-03-26 20:53:58 +0000

    Add some logging to the repository test to try and figure out what is going on in the CI server that makes it fail

--- uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/integrationTest/model/elmo/TestPerson.java
+++ uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/integrationTest/model/elmo/TestPerson.java
@@ -14,6 +14,8 @@ import org.junit.Test;
 
 import uk.ac.osswatch.simal.integrationTest.rdf.BaseRepositoryTest;
 import uk.ac.osswatch.simal.model.IPerson;
+import uk.ac.osswatch.simal.rdf.DuplicateQNameException;
+import uk.ac.osswatch.simal.rdf.SimalRepository;
 import uk.ac.osswatch.simal.rdf.SimalRepositoryException;
 
 public class TestPerson extends BaseRepositoryTest {
@@ -24,7 +26,22 @@ public class TestPerson extends BaseRepositoryTest {
   public static void setUpBeforeClass() throws Exception {
     createRepository();
 
-    developer = repository.getPerson(new QName("http://foo.org/~developer/#me"));
+    developer = repository
+        .getPerson(new QName("http://foo.org/~developer/#me"));
+  }
+
+  @Test
+  public void testAddPersonFromScratch() throws SimalRepositoryException,
+      DuplicateQNameException {
+    QName qname = new QName(SimalRepository.DEFAULT_PERSON_NAMESPACE_URI
+        + "TestingPersonFromScratch");
+    IPerson person;
+    person = repository.createPerson(qname);
+
+    person = repository.getPerson(qname);
+    assertNotNull("Person has not been added to repository", person);
+
+    assertNotNull("Person simalID is incorrectly set", person.getSimalId());
   }
 
   @Test
@@ -50,7 +67,7 @@ public class TestPerson extends BaseRepositoryTest {
     Set<IPerson> knows = developer.getKnows();
     assertNotNull("Should know some people", knows);
     IPerson person = knows.iterator().next();
-    String givenName = (String)person.getFoafGivennames().toArray()[0];
+    String givenName = (String) person.getFoafGivennames().toArray()[0];
     assertTrue("Should know Dan Brickley", givenName.contains("Dan Brickley"));
   }
 
@@ -58,31 +75,33 @@ public class TestPerson extends BaseRepositoryTest {
   public void testGetColleagues() throws SimalRepositoryException {
     Set<IPerson> colleagues = developer.getColleagues();
     assertNotNull(colleagues);
-    assertEquals("Got an incorrect nmber of colleagues", BaseRepositoryTest.getNumberOfParticipants() - 1, colleagues.size());
+    assertEquals("Got an incorrect nmber of colleagues", BaseRepositoryTest
+        .getNumberOfParticipants() - 1, colleagues.size());
     Iterator<IPerson> people = colleagues.iterator();
     while (people.hasNext()) {
       IPerson person = people.next();
-      assertNotNull("No person should have a null ID (see " + person.getQName().toString() + ")", person.getSimalId());
+      assertNotNull("No person should have a null ID (see "
+          + person.getQName().toString() + ")", person.getSimalId());
     }
   }
-  
+
   @Test
   public void testSuppliedSimalId() {
     String id = developer.getSimalId();
     assertEquals("Test developer ID incorrect", "15", id);
-    
+
   }
-  
+
   @Test
   public void testGeneratedSimalId() throws SimalRepositoryException {
     String id = developer.getColleagues().iterator().next().getSimalId();
     assertNotNull("Test developer ID incorrect", id);
   }
-  
+
   @Test
   public void testSeeAlso() {
-    Set<Object> seeAlso= developer.getRdfsSeeAlso();
+    Set<Object> seeAlso = developer.getRdfsSeeAlso();
     assertEquals("There should be a single see also value", 1, seeAlso.size());
-    
+
   }
 }
--- uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/integrationTest/rdf/TestRepository.java
+++ uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/integrationTest/rdf/TestRepository.java
@@ -14,6 +14,8 @@ import javax.xml.namespace.QName;
 
 import org.junit.Test;
 import org.openrdf.concepts.doap.Project;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import uk.ac.osswatch.simal.model.IPerson;
 import uk.ac.osswatch.simal.model.IProject;
@@ -24,16 +26,21 @@ import uk.ac.osswatch.simal.rdf.SimalRepositoryException;
  * 
  */
 public class TestRepository extends BaseRepositoryTest {
+  private static final Logger logger = LoggerFactory
+      .getLogger(TestRepository.class);
 
   @Test
   public void testAddProject() throws SimalRepositoryException {
+    logger.debug("Starting testAddProject()");
     initialiseRepository(false);
     // The default test repository adds projects when it is instantiated
     assertTrue(repository.isInitialised());
+    logger.debug("Finished testAddProject()");
   }
 
   @Test
   public void testFindProject() throws SimalRepositoryException {
+    logger.debug("Starting testFindProject()");
     initialiseRepository(false);
 
     QName qname = new QName("http://foo.org/nonExistent");
@@ -43,10 +50,12 @@ public class TestRepository extends BaseRepositoryTest {
     // test a known valid file
     project = getSimalTestProject(true);
     assertEquals("Simal DOAP Test", project.getName());
+    logger.debug("Finished testFindProject()");
   }
 
   @Test
   public void testGetRdfXml() throws SimalRepositoryException {
+    logger.debug("Starting testGetRdfXML()");
     initialiseRepository(false);
 
     QName qname = new QName(TEST_SIMAL_PROJECT_QNAME);
@@ -60,10 +69,12 @@ public class TestRepository extends BaseRepositoryTest {
     int lastIndexOf = xml.lastIndexOf("<doap:Project");
     assertTrue("XML appears to contain more than one project record",
         indexOf == lastIndexOf);
+    logger.debug("Finished testGetRdfXML()");
   }
 
   @Test
   public void testGetAllProjects() throws SimalRepositoryException, IOException {
+    logger.debug("Starting testGetAllProjects()");
     initialiseRepository(false);
 
     Set<IProject> projects = repository.getAllProjects();
@@ -75,11 +86,13 @@ public class TestRepository extends BaseRepositoryTest {
       project = itrProjects.next();
       assertNotNull(project.getName());
     }
+    logger.debug("Finished testGetAllProjects()");
   }
 
   @SuppressWarnings("unchecked")
   @Test
-  public void testNullQNamehandling() throws SimalRepositoryException {
+  public void testNullQNameHandling() throws SimalRepositoryException {
+    logger.debug("Starting testNullQNameHandling()");
     initialiseRepository(false);
 
     Set<IProject> projects = repository.getAllProjects();
@@ -90,31 +103,38 @@ public class TestRepository extends BaseRepositoryTest {
       project = itrProjects.next();
       assertNotNull("All projects must have a QName", project.getQName());
     }
+    logger.debug("Finished testNullQNameHandling()");
   }
 
   @Test
   public void testGetAllProjectsAsJSON() throws SimalRepositoryException {
+    logger.debug("Starting testGetAllProjectsAsJSON()");
     initialiseRepository(false);
 
     String json = repository.getAllProjectsAsJSON();
     assertTrue("JSON file does not appear to be correct", json
         .startsWith("{ \"items\": ["));
     assertTrue("JSON file does not appear to be correct", json.endsWith("]}"));
+    logger.debug("Finished testGetAllProjectsAsJSON()");
   }
 
   @Test
   public void testRemove() throws SimalRepositoryException {
+    logger.debug("Starting testRemove()");
     repository.remove(new QName(TEST_SIMAL_PROJECT_QNAME));
     Project project = getSimalTestProject(true);
     assertNull("Failed to remove the test project", project);
+    logger.debug("Finished testRemove()");
   }
-  
+
   @Test
   public void testFindPersonById() throws SimalRepositoryException {
+    logger.debug("Starting testFindPersonByID()");
     initialiseRepository(false);
-    
+
     IPerson person = repository.findPersonById("15");
     assertNotNull(person);
     assertEquals("developer", person.getFoafGivennames().toString());
+    logger.debug("Finished testFindPersonByID()");
   }
 }
