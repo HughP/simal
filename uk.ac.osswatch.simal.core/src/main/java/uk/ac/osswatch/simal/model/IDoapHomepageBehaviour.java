08201d389a5d976f3a1dd14108a3d1c604490793
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-03-30 02:48:13 +0000

    - Get URL of home page (ISSUE 96, partial) - @refactor home page tests into own class

--- uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/model/IDoapHomepageBehaviour.java
+++ uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/model/IDoapHomepageBehaviour.java
@@ -1,8 +1,19 @@
 package uk.ac.osswatch.simal.model;
 
+import java.net.URL;
+
+import uk.ac.osswatch.simal.rdf.SimalRepositoryException;
+
 /**
  * Behaviours to attach to a homepage resource.
  *
  */
 public interface IDoapHomepageBehaviour extends IDoapResourceBehaviour{
+  
+  /**
+   * Get the URL for this homepage.
+   * @return
+   * @throws SimalRepositoryException
+   */
+  public URL getURL() throws SimalRepositoryException;
 }
--- uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/model/elmo/DoapHomepageBehaviour.java
+++ uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/model/elmo/DoapHomepageBehaviour.java
@@ -1,9 +1,13 @@
 package uk.ac.osswatch.simal.model.elmo;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openrdf.elmo.annotations.rdf;
 
 import uk.ac.osswatch.simal.model.IDoapHomepage;
 import uk.ac.osswatch.simal.model.IDoapHomepageBehaviour;
+import uk.ac.osswatch.simal.rdf.SimalRepositoryException;
 
 @rdf("http://usefulinc.com/ns/doap#homepage")
 public class DoapHomepageBehaviour extends DoapResourceBehaviour implements IDoapHomepageBehaviour {
@@ -14,5 +18,18 @@ public class DoapHomepageBehaviour extends DoapResourceBehaviour implements IDoa
    */
   public DoapHomepageBehaviour(IDoapHomepage homepage) {
     super(homepage);
+  }
+
+  /**
+   * Get the URL for this homepage.
+   */
+  public URL getURL() throws SimalRepositoryException {
+    String ns = elmoEntity.getQName().getNamespaceURI();
+    String local = elmoEntity.getQName().getLocalPart();
+    try {
+      return new URL(ns + local);
+    } catch (MalformedURLException e) {
+      throw new SimalRepositoryException("Unable to create homepage URL", e);
+    }
   }
 }
