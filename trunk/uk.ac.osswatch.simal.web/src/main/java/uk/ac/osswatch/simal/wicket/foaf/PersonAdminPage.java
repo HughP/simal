834c5f06972db80e343aaa272300f3adf36859f0
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-04-11 20:18:53 +0000

    add a person admin page (all it does right now is list people)

---
+++ trunk/uk.ac.osswatch.simal.web/src/main/java/uk/ac/osswatch/simal/wicket/foaf/PersonAdminPage.java
@@ -0,0 +1,28 @@
+package uk.ac.osswatch.simal.wicket.foaf;
+
+import org.apache.wicket.markup.html.WebPage;
+
+import uk.ac.osswatch.simal.rdf.SimalRepositoryException;
+import uk.ac.osswatch.simal.wicket.BasePage;
+import uk.ac.osswatch.simal.wicket.ErrorReportPage;
+import uk.ac.osswatch.simal.wicket.UserReportableException;
+import uk.ac.osswatch.simal.wicket.panel.PersonListPanel;
+
+/**
+ * This page provides facilities to manage people stored 
+ * in the simal repository.
+ */
+public class PersonAdminPage extends BasePage {
+
+  public PersonAdminPage() {
+    try {
+      add(new PersonListPanel("personList"));
+    } catch (SimalRepositoryException e) {
+      UserReportableException error = new UserReportableException(
+          "Unable to get people from the repository",
+          PersonAdminPage.class, e);
+      setResponsePage(new ErrorReportPage(error));
+    }
+  }
+}
+
