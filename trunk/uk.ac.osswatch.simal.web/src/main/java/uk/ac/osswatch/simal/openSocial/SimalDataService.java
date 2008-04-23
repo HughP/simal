0ee93948845cb268435e16a7df26895abd412aec
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-04-23 00:42:11 +0000

    Shindig no longer needs to be included via svn:external
    However, our open social stuff doesn't work again due to a major refactoring of the Shindig code.

--- trunk/uk.ac.osswatch.simal.web/src/main/java/uk/ac/osswatch/simal/openSocial/SimalDataService.java
+++ trunk/uk.ac.osswatch.simal.web/src/main/java/uk/ac/osswatch/simal/openSocial/SimalDataService.java
@@ -1,7 +1,16 @@
 package uk.ac.osswatch.simal.openSocial;
 
+import org.apache.shindig.social.samplecontainer.XmlStateFileFetcher;
+
+import com.google.inject.Inject;
+
 public class SimalDataService extends
     org.apache.shindig.social.samplecontainer.BasicDataService {
+
+  @Inject
+  public SimalDataService(XmlStateFileFetcher fetcher) {
+    super(fetcher);
+  }
 
 }
 
