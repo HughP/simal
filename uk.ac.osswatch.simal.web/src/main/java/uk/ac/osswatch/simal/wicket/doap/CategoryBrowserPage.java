df9486a8d09dc5c1d04aa682370ba8cd81fa15b5
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-04-23 00:33:09 +0000

    Add category browser page

---
+++ uk.ac.osswatch.simal.web/src/main/java/uk/ac/osswatch/simal/wicket/doap/CategoryBrowserPage.java
@@ -0,0 +1,27 @@
+package uk.ac.osswatch.simal.wicket.doap;
+
+import uk.ac.osswatch.simal.rdf.SimalRepositoryException;
+import uk.ac.osswatch.simal.wicket.BasePage;
+import uk.ac.osswatch.simal.wicket.ErrorReportPage;
+import uk.ac.osswatch.simal.wicket.UserReportableException;
+import uk.ac.osswatch.simal.wicket.panel.CategoryListPanel;
+
+/**
+ * This page provides facilities to browse stored 
+ * in the simal repository.
+ */
+public class CategoryBrowserPage extends BasePage {
+  private static final long serialVersionUID = -3418218005629173956L;
+
+  public CategoryBrowserPage() {
+    try {
+      add(new CategoryListPanel("categoryList"));
+    } catch (SimalRepositoryException e) {
+      UserReportableException error = new UserReportableException(
+          "Unable to get categories from the repository",
+          CategoryBrowserPage.class, e);
+      setResponsePage(new ErrorReportPage(error));
+    }
+  }
+}
+
