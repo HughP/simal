1ba9a80bdcf233a5e9269a8879424480e2d076bb
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-05-22 21:22:54 +0000

    DeDupe projects based on home page

--- trunk/uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/integrationTest/rdf/BaseRepositoryTest.java
+++ trunk/uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/integrationTest/rdf/BaseRepositoryTest.java
@@ -67,7 +67,7 @@ public abstract class BaseRepositoryTest {
   public static final String TEST_SIMAL_PROJECT_MAILING_LIST_TWO = "Mailing List 2";
 
   public static final int TEST_SIMAL_PROJECT_NUMBER_OF_HOMEPAGES = 2;
-  public static final String TEST_SIMAL_PROJECT_HOMEPAGE_URL_ONE = "http://simal.oss-watch.ac.uk";
+  public static final String TEST_SIMAL_PROJECT_HOMEPAGE_URL_ONE = "http://doapTest.oss-watch.ac.uk";
   public static final String TEST_SIMAL_PROJECT_HOMEPAGE_URL_TWO = "http://code.google.com/p/simal";
   public static final String TEST_SIMAL_PROJECT_HOMEPAGE_NAME_ONE = "Project Home Page";
   public static final String TEST_SIMAL_PROJECT_HOMEPAGE_NAME_TWO = "Developer Home Page";
