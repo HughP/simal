4bd7028ad657c16ca0bd31a19eddbc526bf15bff
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-07-20 22:08:53 +0000

    Handle Homepages in the Jena model

---
+++ branches/uk.ac.osswatch.simal.core-jena/src/main/java/uk/ac/osswatch/simal/model/jena/Homepage.java
@@ -0,0 +1,10 @@
+package uk.ac.osswatch.simal.model.jena;
+
+import uk.ac.osswatch.simal.model.IDoapHomepage;
+
+public class Homepage extends DoapResource implements IDoapHomepage {
+
+  public Homepage(com.hp.hpl.jena.rdf.model.Resource resource) {
+    super(resource);
+  }
+}
