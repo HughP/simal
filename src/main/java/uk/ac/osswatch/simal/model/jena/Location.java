35a49131ee27aabb80a758630d4e6fce4b50b917
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-07-23 22:05:46 +0000

    Add tests and functionality for repository objects

---
+++ src/main/java/uk/ac/osswatch/simal/model/jena/Location.java
@@ -0,0 +1,10 @@
+package uk.ac.osswatch.simal.model.jena;
+
+import uk.ac.osswatch.simal.model.IDoapLocation;
+
+public class Location extends DoapResource implements IDoapLocation {
+
+  public Location(com.hp.hpl.jena.rdf.model.Resource resource) {
+    super(resource);
+  }
+}
