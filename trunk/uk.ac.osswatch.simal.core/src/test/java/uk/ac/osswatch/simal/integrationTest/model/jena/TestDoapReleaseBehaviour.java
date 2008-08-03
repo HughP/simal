b937cfa97b8e9079ad83218422d7bdc5939dac14
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-08-03 19:36:49 +0000

    Ooops - got to change the package names in java too! (see last commit)

--- trunk/uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/integrationTest/model/jena/TestDoapReleaseBehaviour.java
+++ trunk/uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/integrationTest/model/jena/TestDoapReleaseBehaviour.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package uk.ac.osswatch.simal.integrationTest.model.elmo;
+package uk.ac.osswatch.simal.integrationTest.model.jena;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
