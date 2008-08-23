dff4a00a9788cf52213f49386430a4403b6da0e0
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-08-23 01:21:16 +0000

    Test for correct number of people (ISSUE 174)

--- trunk/uk.ac.osswatch.simal.web/src/test/java/uk/ac/osswatch/simal/wicket/TestBase.java
+++ trunk/uk.ac.osswatch.simal.web/src/test/java/uk/ac/osswatch/simal/wicket/TestBase.java
@@ -25,7 +25,7 @@ import uk.ac.osswatch.simal.rdf.io.RDFUtils;
 
 public abstract class TestBase {
   protected static final int NUMBER_OF_TEST_PROJECTS = 6;
-  protected static final int NUMBER_OF_TEST_PEOPLE = 18;
+  protected static final int NUMBER_OF_TEST_PEOPLE = 17;
   public static final String TEST_SIMAL_DEVELOPER_URI = RDFUtils.getDefaultPersonURI("15");  protected static WicketTester tester;
 
   @BeforeClass
