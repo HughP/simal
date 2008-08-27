899919080d13e23abe8b833ff0366706242a824f
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2008-08-27 23:50:38 +0000

    use classloader to resolve resources as we now use complete paths in test data names

--- uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/integrationTest/rdf/TestRepository.java
+++ uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/integrationTest/rdf/TestRepository.java
@@ -220,7 +220,7 @@ public class TestRepository extends BaseRepositoryTest {
     project1 = getSimalTestProject();
     assertNull("Project has not been deleted as expected", project1);
     
-    File testFile = new File(ISimalRepository.class.getResource("/testData/" + ModelSupport.TEST_FILE_URI_WITH_QNAME).toURI());
+    File testFile = new File(ISimalRepository.class.getResource(ModelSupport.TEST_FILE_URI_WITH_QNAME).toURI());
     FileInputStream fis = new FileInputStream(testFile);
     int x= fis.available();
     byte b[]= new byte[x];
