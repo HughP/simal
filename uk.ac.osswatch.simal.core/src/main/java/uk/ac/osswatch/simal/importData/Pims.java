8863fe3a905e63a902734e067926348a07aafe0e
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2009-04-28 00:06:31 +0000

    Add starter code for importing PIMS data from XLS exports.

---
+++ uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/importData/Pims.java
@@ -0,0 +1,63 @@
+package uk.ac.osswatch.simal.importData;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import uk.ac.osswatch.simal.model.IDoapHomepage;
+import uk.ac.osswatch.simal.model.IDoapResource;
+import uk.ac.osswatch.simal.model.IProject;
+import uk.ac.osswatch.simal.model.IResource;
+import uk.ac.osswatch.simal.rdf.DuplicateURIException;
+import uk.ac.osswatch.simal.rdf.ISimalRepository;
+import uk.ac.osswatch.simal.rdf.SimalRepositoryException;
+import uk.ac.osswatch.simal.rdf.SimalRepositoryFactory;
+import uk.ac.osswatch.simal.rdf.io.RDFUtils;
+
+public class Pims {
+	
+	/**
+	 * Create a new PImS importer.
+	 * 
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 * @throws SimalRepositoryException 
+	 * @throws DuplicateURIException 
+	 */
+	public Pims() throws FileNotFoundException, IOException, SimalRepositoryException, DuplicateURIException {
+        // Get data from projects spreadsheet
+        String filename = this.getClass().getResource("test/data/QryProjectsForSimal.xls").getFile();
+        HSSFWorkbook wb = new HSSFWorkbook(new FileInputStream(filename));
+        HSSFSheet sheet = wb.getSheetAt(0);
+        HSSFRow row   = sheet.getRow(1);
+        ISimalRepository repo = SimalRepositoryFactory.getInstance();
+        IProject project = repo.createProject(RDFUtils.getDefaultProjectURI(repo.getNewProjectID()));
+        
+        double id = row.getCell(0).getNumericCellValue();
+        project.addName(row.getCell(2).getStringCellValue());
+        project.setDescription(row.getCell(4).getStringCellValue());
+        IDoapHomepage page = repo.createHomepage(row.getCell(6).getStringCellValue());
+        page.addName("Homepage");
+        project.addHomepage(page);
+        //TODO: capture workpackage info: String projectWorkpackage = row.getCell(5).getStringCellValue();
+        //TODO: capture short name: String shortName = row.getCell(3).getStringCellValue();
+        //TODO: capture programme info: String programmeId = row.getCell(1).getStringCellValue();
+        // TODO: Capture state info: String projectStateName = row.getCell(7).getStringCellValue();
+        // TODO: Capture start date info: String projectStartDate = row.getCell(8).getStringCellValue();
+        // TODO: Capture start date info: String projectEndDate = row.getCell(9).getStringCellValue();
+        
+        // Get data from Institutions spreadsheet
+        filename = this.getClass().getResource("test/data/QryProjectInstitutionsForSimal.xls").getFile();
+        wb = new HSSFWorkbook(new FileInputStream(filename));
+        sheet = wb.getSheetAt(0);
+        row   = sheet.getRow(1);
+        //String institutionId = row.getCell(0).getStringCellValue();
+        //String institutionName = row.getCell(1).getStringCellValue();
+        //String institutionProjectId = row.getCell(0).getStringCellValue();
+        
+	}
+}
