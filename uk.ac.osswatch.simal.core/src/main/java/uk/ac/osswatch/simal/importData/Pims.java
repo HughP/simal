2c8e6228c4116ee89458b1cbebb99aedb7d3d11e
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2009-04-28 00:39:16 +0000

    Add missing licence headers

--- uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/importData/Pims.java
+++ uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/importData/Pims.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2009 University of Oxford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ac.osswatch.simal.importData;
 
 import java.io.FileInputStream;
@@ -9,9 +24,7 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 import uk.ac.osswatch.simal.model.IDoapHomepage;
-import uk.ac.osswatch.simal.model.IDoapResource;
 import uk.ac.osswatch.simal.model.IProject;
-import uk.ac.osswatch.simal.model.IResource;
 import uk.ac.osswatch.simal.rdf.DuplicateURIException;
 import uk.ac.osswatch.simal.rdf.ISimalRepository;
 import uk.ac.osswatch.simal.rdf.SimalRepositoryException;
