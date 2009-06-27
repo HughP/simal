22ba907dd1538ebcadffd04f3082764e5deaec63
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2009-06-27 12:48:28 +0000

    Add missing licence header and serialversionid

--- uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/model/jcr/Person.java
+++ uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/model/jcr/Person.java
@@ -1,4 +1,19 @@
 package uk.ac.osswatch.simal.model.jcr;
+/*
+ * Copyright 2007 University of Oxford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -11,7 +26,8 @@ import uk.ac.osswatch.simal.model.IProject;
 import uk.ac.osswatch.simal.rdf.SimalRepositoryException;
 
 public class Person extends Resource implements IPerson {
-	
+	private static final long serialVersionUID = 1L;
+
 	public Person(String uri) throws URISyntaxException {
 		setPath(new URI(uri));
 	}
