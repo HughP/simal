e3a19b94a826e3a9c4d4f3ba0d4e2d0e0ae3c429
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2009-06-28 20:59:50 +0000

    Add missing licence header

--- uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/service/jcr/HomepageService.java
+++ uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/service/jcr/HomepageService.java
@@ -1,11 +1,21 @@
 package uk.ac.osswatch.simal.service.jcr;
-
-import java.net.URISyntaxException;
-
-import com.hp.hpl.jena.rdf.model.Property;
-import com.hp.hpl.jena.rdf.model.Statement;
-import com.hp.hpl.jena.vocabulary.RDF;
-
+/*
+ * Copyright 2007 University of Oxford 
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * 
+ */
 import uk.ac.osswatch.simal.SimalRepositoryFactory;
 import uk.ac.osswatch.simal.model.IDoapHomepage;
 import uk.ac.osswatch.simal.model.jcr.Homepage;
