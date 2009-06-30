576866c90be8a997ab410b8b9c2cb1a16ea49fbf
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2009-06-30 20:49:05 +0000

    Add missing licence headers (why do I forget every time?)

--- uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/model/jena/simal/JenaSimalRepository.java
+++ uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/model/jena/simal/JenaSimalRepository.java
@@ -1,8 +1,8 @@
 package uk.ac.osswatch.simal.model.jena.simal;
 
 /*
+ * Copyright 2007 University of Oxford 
  * 
- Copyright 2007 University of Oxford * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
