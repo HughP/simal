1e40f5d1fe7373f60c437c77d188910b30c3cb6c
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2009-07-03 22:08:49 +0000

    Missing licence header

--- uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/service/jena/JenaCategoryService.java
+++ uk.ac.osswatch.simal.core/src/main/java/uk/ac/osswatch/simal/service/jena/JenaCategoryService.java
@@ -1,4 +1,21 @@
 package uk.ac.osswatch.simal.service.jena;
+/*
+ * Copyright 2007 University of Oxford 
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * 
+ */
 
 import java.util.HashSet;
 import java.util.Set;
@@ -21,7 +38,6 @@ import com.hp.hpl.jena.vocabulary.RDF;
 
 import uk.ac.osswatch.simal.SimalProperties;
 import uk.ac.osswatch.simal.model.IDoapCategory;
-import uk.ac.osswatch.simal.model.IOrganisation;
 import uk.ac.osswatch.simal.model.jena.Category;
 import uk.ac.osswatch.simal.model.jena.simal.JenaSimalRepository;
 import uk.ac.osswatch.simal.rdf.AbstractSimalRepository;
