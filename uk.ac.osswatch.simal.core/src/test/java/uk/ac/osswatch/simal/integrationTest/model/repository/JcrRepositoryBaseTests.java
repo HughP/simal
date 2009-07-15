c57ae68563a1be48287a620b8662257c8d59c90d
ross.gardler <ross.gardler@cad361de-782e-0410-9987-355e4c56c873>
2009-07-15 21:16:13 +0000

    @whitespace organise imports

--- uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/integrationTest/model/repository/JcrRepositoryBaseTests.java
+++ uk.ac.osswatch.simal.core/src/test/java/uk/ac/osswatch/simal/integrationTest/model/repository/JcrRepositoryBaseTests.java
@@ -18,8 +18,8 @@ package uk.ac.osswatch.simal.integrationTest.model.repository;
  */
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Iterator;
