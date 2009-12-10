ad2e4d9c592c10578de267e555e5c38ef0f68746
rgardler@apache.org <rgardler@apache.org@cad361de-782e-0410-9987-355e4c56c873>
2009-12-10 11:46:15 +0000

    Initial import (does nothing useful yet)

---
+++ uk.ac.osswatch.simal.ssmm/src/main/uk/ac/osswatch/simal/ssmm/model/Question.java
@@ -0,0 +1,82 @@
+package uk.ac.osswatch.simal.ssmm.model;
+
+/**
+ * A base question type that allows a free text answer.
+ *
+ */
+public class Question {
+	String text;
+	String details;
+	String answer;
+	
+	/**
+	 * Create a default question that accepts a text response. 
+	 * 
+	 * @param text - the text of the question
+	 * @param details - a description of the question
+	 * @param answer - a default answer
+	 */
+	public Question(String text, String details,
+			String answer) {
+		setText(text);
+		setDetails(details);
+		setAnswer(answer);
+	}
+	
+	/**
+	 * Create a default question that accepts a text response.
+	 * 
+	 * @param text the text of the question
+	 * @param details a description of the question
+	 */
+	public Question(String text, String details) {
+		setText(text);
+		setDetails(details);
+	}
+
+	/**
+	 * Get the text of the question.
+	 * @return
+	 */
+	public String getText() {
+		return text;
+	}
+
+	/**
+	 * Set the text of the question.
+	 */
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	/**
+	 * Get the descriptive details of the question. This can, optionally, contain HTML. 
+	 * @return
+	 */
+	public String getDetails() {
+		return details;
+	}
+
+	/**
+	 * Set the descriptive details of the question. This can, optionally, contain HTML. 
+	 */
+	public void setDetails(String details) {
+		this.details = details;
+	}
+	
+	/**
+	 * Get the answer (if any) provided for this question.
+	 * @return
+	 */
+	public String getAnswer() {
+		return answer;
+	}
+	
+	/**
+	 * Set the answer (if any) provided for this question.
+	 */
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+}
---
+++ uk.ac.osswatch.simal.ssmm/src/test/uk/ac/osswatch/simal/ssmm/model/QuestionTest.java
@@ -0,0 +1,67 @@
+package uk.ac.osswatch.simal.ssmm.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Test base functions of the Question class.
+ *
+ */
+public class QuestionTest {
+	
+	static String QUESTION_TEXT = "Is this a question?";
+	static String QUESTION_DETAILS = "This is just a question for testing.";
+	static String QUESTION_ANSWER = "Yes, of course it's a question.";
+	
+	static Question question;
+	
+	@BeforeClass
+	public static void createQuestion() {
+		question = new Question(QUESTION_TEXT, QUESTION_DETAILS, QUESTION_ANSWER);
+	}
+	
+	@Test
+	public void testCreateQuestion(){
+		Question test = new Question(QUESTION_TEXT, QUESTION_DETAILS);
+		assertEquals("Question text is incorrect", QUESTION_TEXT, test.getText());
+		assertEquals("Description for question is not being set correctly", QUESTION_DETAILS, test.getDetails());
+	}
+	
+	@Test
+	public void testGetText() {
+		assertEquals("Question text is incorrect", QUESTION_TEXT, question.getText());
+	}
+	
+	@Test
+	public void testSetText() {
+		question.setText("testing");
+		assertEquals("Question text is not being set correctly", "testing", question.getText());
+		question.setText(QUESTION_TEXT);
+	}
+	
+	@Test
+	public void testGetAnswer() {
+		assertEquals("Answer to question is incorrect", QUESTION_ANSWER, question.getAnswer());
+	}
+
+	@Test
+	public void testSetAnswer() {
+		question.setAnswer("testing");
+		assertEquals("Answer to question is not being set correctly", "testing", question.getAnswer());
+		question.setAnswer(QUESTION_ANSWER);
+	}
+	
+	@Test
+	public void testGetDescription() {
+		assertEquals("Description for question is incorrect", QUESTION_DETAILS, question.getDetails());
+	}
+	
+	@Test
+	public void testSetDescription() {
+		question.setDetails("testing");
+		assertEquals("Description for question is not being set correctly", "testing", question.getDetails());
+		question.setDetails(QUESTION_DETAILS);
+	}
+}
