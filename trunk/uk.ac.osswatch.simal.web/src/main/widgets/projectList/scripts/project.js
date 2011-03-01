cbd504f062e9063ad808c67338b80484ad6c41ec
rgardler@apache.org <rgardler@apache.org@cad361de-782e-0410-9987-355e4c56c873>
2011-03-01 23:53:52 +0000

    A simple project list widget. It is intended to replace the current project browser panel in some use cases.
    All this does at present is retrieve the projects list via the REST Api and display searchable list of the projects. Clicking on a project in the list opens a simple project details page.
    
    It's not a production ready widget yet, but it is a good start. Things that still need doing include:
    
    - create a setting for the location of the Wookie instance (its currently hard coded as localhost:8888/wookie)
    
    - address performance issues (currently all projects are loaded and all detail pages are created on initialisation)
    
    - add more detail to the detail page

---
+++ trunk/uk.ac.osswatch.simal.web/src/main/widgets/projectList/scripts/project.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2011 University of Oxford
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+var simalRestRoot = "http://localhost:8080/simal-rest/";
+
+/**
+ * The Project model for working with project instances.
+ */ 
+function Project(id, name, shortDesc){
+    this.id = id;
+    this.name = name;
+    this.shortDesc = shortDesc;
+}
+
+var ProjectService = {
+
+  //Find all projects in the repository
+  projects: [],
+  findAll:function() {
+	var loc = simalRestRoot + "allProjects/json";    
+	loc = Widget.proxify(loc);
+	$.getJSON(loc, function(data) {
+		  $('#projectList').empty();
+		  $.each(data, function(key, val) {
+			  $.each(val, function(key, val) {
+			      // Create project and store for future use
+				  var project = new Project(val.simalID, val.name, val.shortdesc);
+			      ProjectService.projects[project.id] = project;
+			      
+			      // Add to list view
+			      $('#projectList').append($("<li/>")
+			    		  				   .append($("<a/>")
+			    		  						   .attr("href", "#" + project.id)
+					      				           .text(project.name)));	        
+		  	      $('#projectList').listview('refresh');
+		  	      
+		  	      // add details page
+		  	      var header = $('<div data-role="header" data-position="fixed"></div>')
+		  	                   .append("<h4>" + project.name + "</h4>")
+		  	      //var footer = $('<div data-role="footer" data-position="fixed"><h4>Footer<h4></div>');
+		  	      var content = $("<div data-role='content'/>")
+	  		          .append($("<h2>" + project.name + "</h2>"))
+	  		          .append($("<p>" + project.shortDesc + "</p>"))
+	  		          
+		  	      $.mobile.pageContainer.append($("<div data-role='page'/>")
+		  	    		  				  .attr("id", project.id)
+		  	    		  				  .attr("data-url", project.id)
+		  	    		  				  .append(header)
+		  	    		  				  .append(content));
+		  	      $('#' + project.id).page();
+
+			  });
+		  });
+		});
+  }
+}
